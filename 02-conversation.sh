68975b99a55a95745da9fbe1f014487c25967c1f
varalakshmi <vv@gmail.com>
2026-06-27 13:25:53 +0530

    adding files

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 : welcome"
+echo "$PERSON1 : welcome1"
+echo "$PERSON2 : welcome2"
+echo "$PERSON2 : welcome3"
---
+++ 03-variables.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+ 
+ echo "pls enter username"
+ read USER_NAME 
+ echo "username is $USER_NAME"
+
+ echo "pls enter password"
+ read -s PASSWORD
+
+ echo "password is:$PASSWORD"
+
+
