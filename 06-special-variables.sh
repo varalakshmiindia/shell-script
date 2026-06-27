4dc37182c8277a920f4d6a85b897ad47bafb6fa0
varalakshmi <vv@gmail.com>
2026-06-27 14:03:48 +0530

    adding files

--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -4,7 +4,7 @@
 echo "all variables passed to script: $@"
  echo "n of variables passed: $#"
 echo "first number: $1"
-# echo "script name: $0"
-# echo "which user running this: $USER"
+echo "script name: $0"
+ echo "which user running this: $USER"
 
 
