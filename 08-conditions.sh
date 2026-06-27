04370876821f9137a76f650229fcc9bcc200a8dc
varalakshmi <vv@gmail.com>
2026-06-27 16:37:18 +0530

    adding files

---
+++ 08-conditions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#gt  - greater than
+#lt  - less than
+#eq  -equal
+#ne  -not equal
+#ge  -greater than or equal
+#le  -less than or equal
+
+if [$number -gt 20 ] ; then
+echo "given number $number is greater to 20"
+elseif [$number -eq 20]; then
+echo "given number $number is equal to 20"
+else
+echo "given number $number is less than to 20"
+fi
+
