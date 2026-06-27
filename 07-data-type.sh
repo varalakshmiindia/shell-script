11a2fd38017a487debc08c2f07962232a904b88b
varalakshmi <vv@gmail.com>
2026-06-27 16:14:30 +0530

    adding files

---
+++ 07-data-type.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+num1=10
+num2=linux
+SUM=$(($num1+$num2))
+echo "sum is $SUM"
+//array
+ movies=("RRR","varanasi","puspa")
+ echo "movies are:{$movies[@]}"
+  echo "movies first:{$movies[0]}"
+    echo "movies second:{$movies[1]}"
+        echo "movies third:{$movies[2]}"
