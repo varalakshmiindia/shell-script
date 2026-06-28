945bba25bf6fad21509e233a0039610fac2699bc
varalakshmi <vv@gmail.com>
2026-06-28 11:26:17 +0530

    log file

--- 10-logs.sh
+++ 10-logs.sh
@@ -12,28 +12,32 @@ exit 1
 fi
 
 
-##echo "i am continuing"
+echo "i am continuing"
 
-# yum install mysql &>> $logs_file
+ yum install mysql &>> $logs_file
 
-# if [$? -eq 0]; then 
-# echo "mysql already installed skip"
-# else
-# echo "installing mysql"
-# yum install mysql -y &>> $logs_file
-# validate mysql $?
-# fi
+ if [$? -eq 0]; then 
+ echo "mysql already installed skip"
+ else
+ echo "installing mysql"
+ yum install mysql -y &>> $logs_file
+ validate mysql $?
+ fi
 
-# yum list install nginx &>> $logs_file
-# if[$? -eq 0]; then
-# echo "nginx alreay installed" 
-# else
-# echo "installing nginx"
-# dnf install nginx -y &>> $logs_file
-# validate nginx $?
+ yum list install nginx &>> $logs_file
+ if[$? -eq 0]; then
+ echo "nginx alreay installed" 
+ else
+ echo "installing nginx"
+ dnf install nginx -y &>> $logs_file
+ validate nginx $?
 
 fi
 #dnf list installed mysql   chek mysql is install or not
 #echo $?
 #dnf list installed mysql-sever
 #mysql --version
+# sudo mkdir /var/log/shell-script
+#ssudo chown -R ec2-user:ec2-user /var/log/shell-script
+# sudo chmod 755 -R  /var/log/shell-script
+
